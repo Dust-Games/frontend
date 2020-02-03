fe8190aeec38cf837c89cef5e71bb13d28552fa4
Anna Kozlova <a.kozlova@sa.media>
2020-02-03 17:00:48 +0300

    front: fix bugs, add i18n

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,12 @@ Vue.mixin(ClickOutside);
 import VueCarousel from "vue-carousel";
 Vue.use(VueCarousel);
 
+/**
+ * Интернализация
+ */
+import VueI18n from 'vue-i18n';
+Vue.use(VueI18n);
+
 new Vue({
   router,
   store,
